be079de19d1483e8f35535b2cc5be71965f60292
ElinVasiliy <ElinVasiliy@mail.ru>
2020-02-10 19:17:57 +0400

    Добавил выбор башен, создание и движение картинки башни за курсором, стрельбу башен, "Смерть" врагов.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Transform exit;
+    [SerializeField]
+    Transform[] wayPoints;
+    [SerializeField]
+    public float navigation;
+    [SerializeField]
+    int health;
+
+    private Transform enemy;
+    Collider2D enemyCollider;
+    private float navigationTime = 0;
+    private int target = 0;
+    bool isDead = false;
+
+    public bool IsDead
+    {
+        get
+        {
+            return isDead;
+        }
+    }
+
+    void Start()
+    {
+        enemy = GetComponent<Transform>();
+        enemyCollider = GetComponent<Collider2D>();
+        Manager.Instance.RegisterEnemy(this);
+    }
+
+    void Update()
+    {
+        if (wayPoints != null && isDead == false)
+        {
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigation)
+            {
+                if (target < wayPoints.Length)
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, wayPoints[target].position, navigationTime);
+                }
+                else
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, exit.position, navigationTime);
+                }
+                navigationTime = 0;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "MoveingPoint")
+        {
+            target += 1;
+        }
+        else if (collision.tag == "Finish")
+        {
+            Manager.Instance.UnregisterEnemy(this);
+        }
+        else if (collision.tag == "Projectile")
+        {
+            Projectile newP = collision.gameObject.GetComponent<Projectile>();
+            EnemyHit(newP.AttackDamage);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public void EnemyHit(int hitPoints)
+    {
+        if (health - hitPoints > 0)
+        {
+            //hurt
+            health -= hitPoints;
+        }
+        else
+        {
+            //die
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        isDead = true;
+        enemyCollider.enabled = false;
+        Manager.Instance.UnregisterEnemy(this);
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : Loader<Manager>
+{
+    [SerializeField]
+    private GameObject spawnPoint;
+    [SerializeField]
+    private GameObject[] enemies;
+    [SerializeField]
+    private int maxEnemiesOnScreen;
+    [SerializeField]
+    private int totalEnemies;
+    [SerializeField]
+    private int enemiesPerSpawn;
+
+    public List<Enemy> EnemyList = new List<Enemy>();
+
+    private const float spawnDelay = 0.5f;
+    
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        if (enemiesPerSpawn > 0 && EnemyList.Count < totalEnemies)
+        {
+            for (int i = 0; i < enemiesPerSpawn; i++)
+            {
+                if (EnemyList.Count < maxEnemiesOnScreen)
+                {
+                    GameObject newEnemy = Instantiate(enemies[0]) as GameObject;
+                    newEnemy.transform.position = spawnPoint.transform.position;
+                }
+            }
+
+            yield return new WaitForSeconds(spawnDelay);
+            StartCoroutine(Spawn());
+        }
+    }
+
+    public void RegisterEnemy(Enemy enemy)
+    {
+        EnemyList.Add(enemy);
+    }
+
+    public void UnregisterEnemy(Enemy enemy)
+    {
+        EnemyList.Remove(enemy);
+        Destroy(enemy.gameObject);
+    }
+
+    public void DestroyEnemies()
+    {
+        foreach (Enemy enemy in EnemyList)
+        {
+            Destroy(enemy.gameObject);
+        }
+        EnemyList.Clear();
+    }
+}
